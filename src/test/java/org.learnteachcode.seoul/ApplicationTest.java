c8f0ec3b9fb988c07a6b06576a495ef29bfdd618
Ben <bsadick@gmail.com>
2017-04-05 18:23:55 +0900

    Added a failing test

---
+++ src/test/java/org.learnteachcode.seoul/ApplicationTest.java
@@ -0,0 +1,16 @@
+package org.learnteachcode.seoul;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hector on 4/5/17.
+ */
+public class ApplicationTest {
+    @Test
+    public void main() throws Exception {
+        fail();
+    }
+
+}
